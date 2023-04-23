4a32772865315a5678f8b7b82c7be917f70b8d18
Mauricio <mau04@outlook.es>
2023-04-22 22:39:52 -0600

    first commit

---
+++ APISistemaVenta/APISistemaVenta/Controllers/DashBoardController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DTO;
+using APISistemaVenta.Utilidad;
+using SistemaVenta.BLL.Servicios;
+
+namespace APISistemaVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashBoardController : ControllerBase
+    {
+        private readonly IDashBoardService _dashBoardService;
+
+        public DashBoardController(IDashBoardService dashBoardService)
+        {
+            _dashBoardService = dashBoardService;
+        }
+
+        [HttpGet]
+        [Route("Resumen")]
+
+        public async Task<IActionResult> Resumen()
+        {
+            var rsp = new Response<DashBoardDTO>();
+
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _dashBoardService.Resumen();
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+            }
+            return Ok(rsp);
+
+        }
+    }
+}
---
+++ APISistemaVenta/SistemaVenta.BLL/Servicios/CategoriaService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+namespace SistemaVenta.BLL.Servicios
+{
+    public class CategoriaService:ICategoriaService
+    {
+        private readonly IGenericRepository<Categoria> _categoriaRepository;
+        private readonly IMapper _mapper;
+
+        public CategoriaService(IGenericRepository<Categoria> categoriaRepository, IMapper mapper)
+        {
+            _categoriaRepository = categoriaRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CategoriaDTO>> Lista()
+        {
+            try
+            {
+                var listaCategorias = await _categoriaRepository.Consultar();
+                return _mapper.Map<List<CategoriaDTO>>(listaCategorias.ToList());
+            }
+            catch { throw; }
+        }
+    }
+}
